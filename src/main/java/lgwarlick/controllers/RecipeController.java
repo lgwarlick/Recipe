1e44fdd7d4fce850a4d6e5cb8c225fd0caa949a9
Logan <lgwarlick@gmail.com>
2020-08-17 18:22:45 -0500

    added thymeleaf tags to show.html, added servings/source/url to bootstrap data

--- src/main/java/lgwarlick/controllers/RecipeController.java
+++ src/main/java/lgwarlick/controllers/RecipeController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class RecipeController {
-    
+
     private final RecipeService recipeService;
 
     public RecipeController(RecipeService recipeService) {
