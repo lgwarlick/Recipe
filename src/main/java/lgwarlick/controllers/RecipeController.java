a76826830a7b682b6ae39e17def4a76f905e29df
Logan <lgwarlick@gmail.com>
2020-09-01 16:35:52 -0500

    Added IngredientController and list.html to display ingredients in a recipe

--- src/main/java/lgwarlick/controllers/RecipeController.java
+++ src/main/java/lgwarlick/controllers/RecipeController.java
@@ -2,10 +2,13 @@ package lgwarlick.controllers;
 
 import lgwarlick.commands.RecipeCommand;
 import lgwarlick.services.RecipeService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+
+@Slf4j
 @Controller
 public class RecipeController {
 
@@ -49,6 +52,9 @@ public class RecipeController {
     @GetMapping
     @RequestMapping("recipe/{id}/delete")
     public String deleteById(@PathVariable String id){
+
+        log.debug("Deleting recipe id: " + id);
+
         recipeService.deleteById(Long.valueOf(id));
         return "redirect:/";
     }
