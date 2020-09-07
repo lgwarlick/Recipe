45fe27db8648bedd65f773eec1a231ee90083f03
Logan <lgwarlick@gmail.com>
2020-09-07 11:03:13 -0500

    Implemented persisting of images to database

--- src/main/java/lgwarlick/services/ImageServiceImpl.java
+++ src/main/java/lgwarlick/services/ImageServiceImpl.java
@@ -1,16 +1,50 @@
 package lgwarlick.services;
 
+import lgwarlick.domain.Recipe;
+import lgwarlick.repositories.RecipeRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
 
 @Slf4j
 @Service
 public class ImageServiceImpl implements ImageService {
 
+    private final RecipeRepository recipeRepository;
+
+    public ImageServiceImpl(RecipeRepository recipeRepository) {
+        this.recipeRepository = recipeRepository;
+    }
+
     @Override
+    @Transactional
     public void saveImageFile(Long recipeId, MultipartFile file) {
 
-        log.debug("Received a file");
+        try {
+            Recipe recipe = recipeRepository.findById(recipeId).get();
+
+            //Multipart file uses primitive byte array
+            //setting up object wrapper array of same size
+            Byte[] byteObjects = new Byte[file.getBytes().length];
+
+            int i = 0;
+
+            //converting from primitive to wrapper
+            for (byte b : file.getBytes()) {
+                byteObjects[i++] = b;
+            }
+
+            recipe.setImage(byteObjects);
+
+            recipeRepository.save(recipe);
+
+        } catch (IOException e) {
+            log.error("Error occurred", e);
+            e.printStackTrace();
+        }
+
     }
 }
